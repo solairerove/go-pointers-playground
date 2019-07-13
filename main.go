676b031872edcb428dd586cbeb949b560d94b67e
Mikita Kryvitski <solairerove@gmail.com>
2019-07-13 15:11:46 +0300

    feature: add simple pointers example

--- main.go
+++ main.go
@@ -6,4 +6,21 @@ import (
 
 func main() {
 	fmt.Println("Pointers")
+
+	a := 42
+	fmt.Println(a)
+	// address
+	fmt.Println(&a)
+
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", &a)
+
+	b := &a
+	fmt.Println(b)
+	// value stored at an address
+	fmt.Println(*b)
+	fmt.Println(*&a)
+
+	*b = 43
+	fmt.Println(a)
 }
